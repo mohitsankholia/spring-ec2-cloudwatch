21362a0a6ff55bc089c1fad3e17cc3fb2bb307d8
Mohit <97430126+mohitsankholia@users.noreply.github.com>
2023-04-19 20:52:51 +0530

    Update SpringController.java

--- src/main/java/com/mohitsankholia/SpringSecurity/controller/SpringController.java
+++ src/main/java/com/mohitsankholia/SpringSecurity/controller/SpringController.java
@@ -1,39 +1,53 @@
 package com.mohitsankholia.SpringSecurity.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringController {
-
 	
+	private static final Logger LOGGER = LogManager.getLogger(SpringController.class);
+
 	@GetMapping("/")
 	private String home() {
 		
+		LOGGER.info("home url hitted");
+
 		return ("<h1>welcome to new home</h1>");
 	}
-	
+
 	@GetMapping("/user/one")
 	private String userOne() {
 		
+		LOGGER.info("user one url hitted");
+
 		return ("<h1>welcome to User One</h1>");
 	}
-	
+
 	@GetMapping("/user/two")
 	private String userTwo() {
 		
+		LOGGER.info("user two url hitted");
+
 		return ("<h1>welcome to user two</h1>");
 	}
-	
+
 	@GetMapping("/admin/one")
 	private String adminOne() {
 		
+		LOGGER.info("admin one url hitted");
+
 		return ("<h1>welcome to admin one</h1>");
 	}
-	
+
 	@GetMapping("/admin/two")
 	private String adminTwo() {
 		
+		LOGGER.info("admin two url hitted");
+
 		return ("<h1>welcome to admin two</h1>");
 	}
 }
